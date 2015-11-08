72556e688b63b68cf4f8b06228bdef6ad1e4bbe8
whiDev <whiplashoo721@gmail.com>
2015-11-08 23:31:24 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+minifycss = require('gulp-minify-css'),
+jshint = require('gulp-jshint'),
+uglify = require('gulp-uglify'),
+rename = require('gulp-rename'),
+concat = require('gulp-concat'),
+notify = require('gulp-notify'),
+livereload = require('gulp-livereload'),
+del = require('del');
+
+
+
+
+
+
+
+gulp.task('default', function() {
+    gulp.start('styles','scripts');
+});
+
+gulp.task('styles', function() {
+  return gulp.src('css/*.css')
+  .pipe(minifycss({compatibility: 'ie8'}))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('dist/css/'));
+});
+gulp.task('scripts', function() {
+  return gulp.src('js/*.js')
+  .pipe(concat('all.js'))
+  .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('dist/js'));
+});
+gulp.task('watch', function() {
+
+  // Watch .css files
+  gulp.watch('css/*.css', ['styles']);
+
+  // Watch .js files
+  gulp.watch('js/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+});
---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	// $(".project").click(function(){
+	// 	window.location = $(this).find("a:first").attr("href");
+	// 	return false;
+	// });
+
+	// // Show URL on Mouse Hover
+	// $(".project").hover(function () {
+	// 	window.status = $(this).find("a:first").attr("href");
+	// }, function () {
+	// 	window.status = "";
+	// });
+
+/*======= Skillset *=======*/
+
+$('.level-bar-inner').css('width', '0');
+
+$(window).on('load', function() {
+
+	$('.level-bar-inner').each(function() {
+
+		var itemWidth = $(this).data('level');
+
+		$(this).animate({
+			width: itemWidth
+		}, 800);
+
+	});
+
+});
+
+$('#contact').click(function(){
+	$(this).html('Email me at: whiplashoo721@gmail.com');
+});
+})
+
+var doABarrelRoll = function(){
+	var a="-webkit-",b='transform:rotate(1turn);',c='transition:2s;';
+	document.head.innerHTML+= '<style id="barell">body{'+a+b+a+c+b+c;
+}
+
+  
+
+$('#roll').click(function(){
+	doABarrelRoll();
+	setTimeout(function(){$('#barell').remove();},5000);
+});
+
+$('#roll').mouseover(function(){
+	$('.clickme').css('color', '#33c3f0');
+}).mouseout(function(){
+	$('.clickme').css('color', '#2f2e33');
+});
